2effc5ea7953e631a126f43523769aaef69e900e
Darryl Rettig <darryl@donationmatch.com>
2017-06-14 14:44:26 -0700

    component

--- src/QueryBuilderRule.vue
+++ src/QueryBuilderRule.vue
@@ -17,7 +17,7 @@
       <input :class="{ 'form-control': styled }" v-if="rule.inputType === 'number'" type="number" v-model="query.value"></input>
 
       <template v-if="isCustomComponent">
-        <component :value="query.value" @input="updateQuery" is="query.rule"></component>
+        <component :value="query.value" @input="updateQuery" is="{{query.rule}}"></component>
       </template>
 
       <div class="checkbox" v-if="rule.inputType === 'checkbox'">
